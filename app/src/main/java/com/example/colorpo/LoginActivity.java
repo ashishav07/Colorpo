d36dbaa1b06ff0251c015ca419bcd3f896619689
Ashish Vyas <17ucs039@lnmiit.ac.in>
2019-09-03 22:08:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/colorpo/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.colorpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button regButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        regButton = (Button)findViewById(R.id.register);
+        regButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this,RegisterActivity.class);
+    }
+}
