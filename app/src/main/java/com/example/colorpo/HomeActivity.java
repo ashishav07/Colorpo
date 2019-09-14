9397b7847304613f4b28c4a47b60e3b4218b6856
ashutoshdumiyan <ashutosh.dumiyan@gmail.com>
2019-09-14 12:46:08 +0530

    added three dot menu and demo search button

--- app/src/main/java/com/example/colorpo/HomeActivity.java
+++ app/src/main/java/com/example/colorpo/HomeActivity.java
@@ -8,7 +8,9 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -19,6 +21,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // To show the button of navigation drawer
         setContentView(R.layout.activity_home);
         drawer = findViewById(R.id.drawer_layout);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -27,8 +31,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        // To handle clicks on navigation menu items
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -52,6 +58,30 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    // To show the three dot menu on right hand side
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out:
+                // Put sign out code here
+                Toast.makeText(this, "Sign Out", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search:
+                // Put search code here
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                break;
+        }
         return true;
     }
 }
