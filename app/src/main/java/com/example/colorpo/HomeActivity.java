fb8a710d67d9fae0cb8b15d83ac9eaa239b8aa2a
ashutoshdumiyan <ashutosh.dumiyan@gmail.com>
2019-10-11 01:33:43 +0530

    edited profile fragment

--- app/src/main/java/com/example/colorpo/HomeActivity.java
+++ app/src/main/java/com/example/colorpo/HomeActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.DialogInterface;
 import android.content.Intent;
